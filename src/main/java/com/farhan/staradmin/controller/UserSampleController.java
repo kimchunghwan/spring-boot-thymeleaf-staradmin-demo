0b77e9aeaf7345a06d766d599a4cb532e1987806
junghangil <arnudora@gmail.com>
2023-10-20 10:59:53 +0900

    edit

--- src/main/java/com/farhan/staradmin/controller/UserSampleController.java
+++ src/main/java/com/farhan/staradmin/controller/UserSampleController.java
@@ -25,14 +25,10 @@ public class UserSampleController {
     public String selectAllUsers(Model model, @RequestParam(required = false) Boolean updated) {
         List<UserSample> users = userSampleService.selectAllUsers();
 
-        // 로깅 추가: 사용자 목록을 로그에 출력
         logger.info("Fetched users: {}", users);
 
-        if (Boolean.TRUE.equals(updated)) {
-            model.addAttribute("showPopup", true);
-        }
-
         model.addAttribute("users", users);
+
         return "pages/user-sample-list";
     }
 
@@ -40,17 +36,17 @@ public class UserSampleController {
     public String selectOneUserById(@PathVariable Long id, Model model) {
         UserSample user = userSampleService.selectOneUserById(id);
 
-        // 로깅 추가: 사용자 목록을 로그에 출력
         logger.info("Fetched user: {}", user);
 
         model.addAttribute("user", user);
+
         return "pages/user-sample-detail";
     }
 
     // insertUser
 
     @PostMapping("/updateUser")
-    public String updateUser(@RequestParam("mailPrefix") String mailPrefix, @RequestParam("mailSuffix") String mailSuffix,  UserSample userSample, Model model) {
+    public String updateUser(@RequestParam("mailPrefix") String mailPrefix, @RequestParam("mailSuffix") String mailSuffix,  UserSample userSample) {
         // Mail
         String mail = mailPrefix + "@" + mailSuffix;
         userSample.setMail(mail);
@@ -60,24 +56,17 @@ public class UserSampleController {
 
         userSampleService.updateUser(userSample);
 
-        // 로깅 추가: 업데이트된 사용자 정보를 로그에 출력
         logger.info("Updated user: {}", userSample);
-
-        model.addAttribute("showUpdatePopup", true);
 
         return "redirect:/pages/user-sample-list?updated=true";
     }
 
     @PostMapping("/deleteUser/{id}")
-    public String deleteUserById(@PathVariable Long id, Model model) {
+    public String deleteUserById(@PathVariable Long id) {
         userSampleService.deleteUserById(id);
 
-        // 로깅 추가: 삭제된 사용자 정보를 로그에 출력
         logger.info("Deleted user: {}", id);
 
-        model.addAttribute("showDeletePopup", true);
-
-        // 삭제 완료 후 유저 리스트 페이지로 리다이렉트합니다.
         return "redirect:/pages/user-sample-list?deleted=true";
     }
 
